d7d30f40b7055bc1687076666f6612d3454bb566
usama rehman <iusamarehman96@gmail.com>
2022-09-15 16:54:35 +0500

    completed cart functionality with logout func

--- eComm-en/app/Http/Controllers/ProductController.php
+++ eComm-en/app/Http/Controllers/ProductController.php
@@ -5,6 +5,10 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
+use App\Models\Order;
+
+use Session;
+use Illuminate\Support\Facades\DB;
 class ProductController extends Controller
 {
     function index()
@@ -20,17 +24,24 @@ class ProductController extends Controller
       public function addToCart(Request $req)
     {
        
-        if( $req->session()->has('user'))
+         if($req->session()->has('user'))
         {
-          $cart = new Cart;
-          $cart->user_id=$req->session()->get('user')['id'];
-          $cart->product_id=$req->product_id;
-          $cart->save();
-          return redirect('/');
-                }
-        else 
+           $cart= new Cart;
+           $cart->user_id=$req->session()->get('user')['id'];
+           $cart->product_id=$req->product_id;
+           $cart->save();
+           return redirect('/');
+
+        }
+        else
         {
             return redirect('/login');
         }
+    }
+  public static  function cartItem()
+    {
+     $userId=Session::get('user')['id'];
+     return Cart::where('user_id',$userId)->count();
+     
     }
 }
--- eComm-en/resources/views/header.blade.php
+++ eComm-en/resources/views/header.blade.php
@@ -1,5 +1,16 @@
+
+<?php 
+use App\Http\Controllers\ProductController;
+
+$total=0;
+if(Session::has('user'))
+{
+  $total= ProductController::cartItem();
+
+}
+?>
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="/">E-com</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -19,11 +30,33 @@
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
      
-  </div>
- <span>Cart</span>
-<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
+
+</ul> 
+ <ul class="nav navbar-nav navbar-right">
+  <ul>
+  <li><a href="">cart {{$total}} </a>  </li>
+ 
+</ul>
+<ul>
+   @if(Session:: has('user'))
+   <li class="dropdown">
+        <a class="dropdown-toggle" data-toggle="dropdown" href="#">{{Session::get('user')['name']}}
+        <span class="caret"></span></a>
+        <ul class="dropdown-menu">
+          <li><a href="/logouts">Logout</a></li>
+  
+        </ul>
+      </li>
+</ul>
+
+@else 
+ <li><a href="/login">Login </a>  </li>
+@endif
+ </ul>
+ 
+<!-- <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cart" viewBox="0 0 16 16">
   <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l1.313 7h8.17l1.313-7H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z"/>
-</svg>
+</svg> -->
    
   
 </nav>
