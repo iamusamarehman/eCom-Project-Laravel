d789dfcd9e67581b722bbecf098168a1d80a7270
usama rehman <iusamarehman96@gmail.com>
2022-09-16 12:29:48 +0500

    order struture done and order table created

--- eComm-en/app/Http/Controllers/ProductController.php
+++ eComm-en/app/Http/Controllers/ProductController.php
@@ -62,5 +62,17 @@ class ProductController extends Controller
   Cart::destroy($id);
   return redirect('cartlist');
  }
+ function orderNow()
+ {
+    $userId=Session::get('user')['id'];
+      $total= $products= DB::table('cart')
+        ->join('products','cart.product_id','=','products.id')
+        ->where('cart.user_id',$userId)
+        ->select('products.*','cart.id as cart_id')
+        ->sum('products.price');
+
+        return view('Ordernow',['total'=> $total]); //first total is a varibale 2nd one come from database total value variable 
+
+ }
 
 }
